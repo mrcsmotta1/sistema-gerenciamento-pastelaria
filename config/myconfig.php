a046b87ae3064b3971131f75de28f71b8ddaec71
Marcos Motta <mrcsmotta1@gmail.com>
2023-11-05 17:24:14 -0300

    Creating Config with allowed image extensions for products

---
+++ config/myconfig.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * MyClass File Doc Comment
+ * php version 8.1
+ *
+ * @category Config
+ * @package  Config
+ * @author   Marcos Motta <mrcsmotta1@gmail.com>
+ * @license  MIT License
+ * @link     https://github.com/mrcsmotta1/sistema-gerenciamento-pastelaria
+ */
+
+return [
+    'extensions' => [
+        'image/jpeg' => 'jpg',
+        'image/png' => 'png',
+        'image/gif' => 'gif',
+    ],
+];
