f6780daf1eca817b2ac0c0ef99c4f3d57f85b590
Marcos Motta <mrcsmotta1@gmail.com>
2023-11-07 21:27:02 -0300

    Changing test configuration in sqlite

---
+++ app/Providers/PhoneFormatProvider.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * MyClass File Doc Comment
+ * php version 8.1
+ *
+ * @category Providerssss
+ * @package  App\Providers
+ * @author   Marcos Motta <mrcsmotta1@gmail.com>
+ * @license  MIT License
+ * @link     https://github.com/mrcsmotta1/sistema-gerenciamento-pastelaria
+ */
+
+namespace App\Providers;
+
+use Faker\Provider\Base;
+
+/**
+ * Arquivo Class CustomerController.
+ *
+ * Controller Customer.
+ *
+ * @phpcs
+ * php version 8.1
+ *
+ * @category Providers
+ * @package  App\Providers
+ * @author   Marcos Motta <mrcsmotta1@gmail.com>
+ * @license  MIT License
+ * @link     https://github.com/mrcsmotta1/sistema-gerenciamento-pastelaria
+ */
+class PhoneFormatProvider extends Base
+{
+    /**
+     * Generate a random phone number with a specific format.
+     *
+     * @return string A random phone number with the specified format.
+     */
+    public function phoneNumberWithFormat()
+    {
+        $formats = [
+            '(%%)9 %%%%-%%%%',
+            '(%%)9%%%-%%%%',
+            '(%%)%%%%-%%%%',
+        ];
+
+        $format = $this->randomElement($formats);
+
+        return $this->numerify($format);
+    }
+}
